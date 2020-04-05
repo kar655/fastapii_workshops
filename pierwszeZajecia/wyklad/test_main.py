6d8d2d7f8787992c9b544f7b61ce17a81d879f3f
kar655 <karol123zag@gmail.com>
2020-04-05 15:50:01 +0200

    Add fisrt post

--- pierwszeZajecia/wyklad/test_main.py
+++ pierwszeZajecia/wyklad/test_main.py
@@ -1,7 +1,11 @@
 
 from fastapi.testclient import TestClient
 
-from main import app
+import pytest
+
+from main import app, HelloResp
+
+
 
 
 client = TestClient(app)
@@ -11,3 +15,16 @@ def test_read_main():
 	response = client.get("/")
 	assert response.status_code == 200
 	assert response.json() == {"message": "Hello World"}
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Ola", "Jan Kowalski", "Żółty"])
+def test_hello_name(name):
+	response = client.get(f"/hello/{name}")
+	assert response.status_code == 200
+	# print(response.text)
+	assert response.json() == {"msg":f"Hello {name}"}
+
+def test_receive_something():
+	response = client.post("/dej/mi/coś", json={'first_key': 'some_value'})
+	assert response.json() == {"received": {'first_key': 'some_value'},
+								"constant_data": "python jest super"}
